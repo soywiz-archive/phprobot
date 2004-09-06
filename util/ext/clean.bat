78d1e3bac38d16789ee10b6e9372863e0948f245
phpwiz2 <phpwiz2>
2004-09-06 19:14:09 +0000

    *** empty log message ***

--- util/ext/clean.bat
+++ util/ext/clean.bat
@@ -1,12 +1,12 @@
 @echo off
-echo Deleting grfio intermediate files...
-@del /q grfio\grfio.exp
-@del /q grfio\grfio.lib
-@del /q grfio\grfio.ncb
-@del /q grfio\grfio.obj
-@del /q grfio\grfio.opt
-@del /q grfio\grfio.plg
-@del /q grfio\vc60.idb
+rem echo Deleting grfio intermediate files...
+@rem del /q grfio\grfio.exp
+@rem del /q grfio\grfio.lib
+@rem del /q grfio\grfio.ncb
+@rem del /q grfio\grfio.obj
+@rem del /q grfio\grfio.opt
+@rem del /q grfio\grfio.plg
+@rem del /q grfio\vc60.idb
 echo Deleting path intermediate files...
 @del /q path\path.exp
 @del /q path\path.lib
