d7653c38d9243239a02c2b0856671b47cdded01f
phpwiz2 <phpwiz2>
2005-02-17 14:21:34 +0000

    Testing

---
+++ src/Test/WinbinderFreeImage.php
@@ -0,0 +1,10 @@
+<?php
+	import('Winbinder.Winbinder');
+	import('Winbinder.FreeImage');
+
+	//wb_create_window(NULL, PopupWindow, "Hello world!", 480, 320);
+	//wb_main_loop();
+
+	//echo FreeImage_GetVersion();
+	FreeImage_Load(FIF_PNG, 'e:\\lunea\\data\\maps_imgs\\alberta.png');
+?>
