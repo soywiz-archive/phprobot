17c6ae81cf24668e0ad1a1aed7b00734f53aeaf2
phpwiz2 <phpwiz2>
2005-02-19 12:02:00 +0000

    Grf Find

--- ext/src/php_grf/grf.c
+++ ext/src/php_grf/grf.c
@@ -8,6 +8,8 @@
 
 #include "php_grf.h"
 
+#include "gpattern/gpattern.h"
+
 static int le_grfp;
 
 static zend_class_entry *grf_class_entry_ptr;
@@ -18,7 +20,9 @@ static zend_function_entry grf_functions[] = {
 
 static void destroy_grf_resource(zend_rsrc_list_entry *resource TSRMLS_DC) {
 	//grf_close((Grfptr)resource->ptr);
-	grf_free((Grfptr)resource->ptr);
+
+	//if (!resource->ptr)
+		grf_free((Grfptr)resource->ptr);
 }
 
 
@@ -93,18 +97,51 @@ PHP_METHOD(Grf, __construct) {
 	convert_to_string_ex(filename);
 	mygrf = grf_callback_open(Z_STRVAL_PP(filename), "rb", NULL, NULL);
 
-	ret = zend_list_insert(mygrf, le_grfp);
-	
-	object_init_ex(getThis(), grf_class_entry_ptr);
-	add_property_resource(getThis(), "grf", ret);
-	add_property_long(getThis(), "count", mygrf->nfiles);
-	zend_list_addref(ret);
-
+	if (!mygrf) {
+		object_init_ex(getThis(), grf_class_entry_ptr);
+		add_property_long(getThis(), "count", 0);
+		php_error_docref(NULL TSRMLS_CC, E_NOTICE, "File not exists", "Grf");
+	} else {
+		ret = zend_list_insert(mygrf, le_grfp);
+		
+		object_init_ex(getThis(), grf_class_entry_ptr);
+		add_property_resource(getThis(), "grf", ret);
+		add_property_long(getThis(), "count", mygrf->nfiles);
+		zend_list_addref(ret);
+	}
 	//RETURN_FALSE;
 }
 
+int inline grffile_is_dir(GrfFile *gf) {
+	return
+		(gf->compressed_len_aligned == 0x0714) &&
+		(gf->compressed_len         == 0x0449) &&
+		(gf->real_len               == 0x055C) &&
+		(gf->pos                    == 0x058A);
+}
+
 PHP_METHOD(Grf, Find) {
-	RETURN_FALSE;
+	zval **pattern;
+	unsigned int n, t;
+	Grfptr mygrf;
+
+	if (ZEND_NUM_ARGS() != 1 || zend_get_parameters_ex(1, &pattern) == FAILURE) WRONG_PARAM_COUNT;	
+
+	convert_to_string_ex(pattern);
+
+	mygrf = getGrf(getThis() TSRMLS_CC);
+
+	array_init(return_value);
+	t = mygrf->nfiles;
+	for (n = 0; n < t; n++) {
+		if (g_pattern_match_simple(Z_STRVAL_PP(pattern), mygrf->files[n].name)) {
+			if (grffile_is_dir(&mygrf->files[n])) {
+				continue;
+			} else {
+				add_index_string(return_value, n, mygrf->files[n].name, 1);
+			}
+		}
+	}
 }
 
 PHP_METHOD(Grf, ReadIndex) {
--- src/Test/Grf.php
+++ src/Test/Grf.php
@@ -1,9 +1,14 @@
 <?php
 	Import('Grf.Grf');
 
-	$grf = new Grf('Test.grf');
+    //$grf = new Grf('Test.grf');
+    $grf = new Grf('e:\\juegos\\ragnarok\\sprodata.grf');
 
 	print_r($grf);
 
 	echo $grf->Read('data\\test.txt');
+
+	echo "\n";
+
+	print_r($grf->Find('*.spr'));
 ?>
