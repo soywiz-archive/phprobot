524556b94fbf02495336cdf9380d10bc34da1f63
phpwiz2 <phpwiz2>
2005-02-19 12:10:32 +0000

    Grf Find

--- src/Test/Grf.php
+++ src/Test/Grf.php
@@ -1,14 +1,14 @@
 <?php
 	Import('Grf.Grf');
 
-    //$grf = new Grf('Test.grf');
-    $grf = new Grf('e:\\juegos\\ragnarok\\sprodata.grf');
+    $grf = new Grf('Test.grf');
+    //$grf = new Grf('e:\\juegos\\ragnarok\\data.grf');
 
 	print_r($grf);
 
-	echo $grf->Read('data\\test.txt');
+	//echo $grf->Read('data\\test.txt');
 
-	echo "\n";
+	//echo "\n";
 
-	print_r($grf->Find('*.spr'));
+	//print_r($grf->Find('*.gat'));
 ?>
